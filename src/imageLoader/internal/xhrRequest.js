c9d0b2cd1e78e53024d2add88793e3a874e027bd
Jorge González <steinkel@gmail.com>
2021-05-18 12:01:59 +0100

    fix var access

--- src/imageLoader/internal/xhrRequest.js
+++ src/imageLoader/internal/xhrRequest.js
@@ -108,7 +108,7 @@ function xhrRequest (url, imageId, headers = {}, params = {}) {
 
   // @todo move these options to user based flags for localcache and preventRedirect for IE only
   var useLocalCache = false;
-  if (typeof userEnableLocalCache !== 'undefined' && userEnableLocalCache) {
+  if (typeof window.AppConfig !== 'undefined' && typeof window.AppConfig.userEnableLocalCache !== 'undefined' && userEnableLocalCache) {
     useLocalCache = true;
   }
   var preventRedirectCORS = false;
